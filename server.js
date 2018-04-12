47ffe4170ef40ab1b51b6bb6f2ceb367008b198f
Ivan <ivanms1@gmail.com>
2018-04-12 15:27:05 +0700

    create and populate top 25 playlist

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/login', function(req, res) {
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
-      scope: 'user-read-private user-read-email user-top-read',
+      scope: 'user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private playlist-modify-private',
       redirect_uri
     }))
 })
